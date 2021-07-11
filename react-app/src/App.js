5ddea14208effe846ef6b3298fb2d8007cd65449
jinseonghee <wlstjdgml0610@gmail.com>
2021-07-12 02:06:17 +0900

    feat - event state and props

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -9,7 +9,9 @@ class App extends Component {
   constructor(props) {
     super(props); //Component가 실행될대 Constructor라는 함수가 제일 먼저 실행돼서 초기화 담당
     this.state = { //state를 초기화
-      subject: {title : 'WEB', sub : 'World wide web!'},
+      mode: 'welcome',
+      subject: {title: 'WEB', sub: 'World wide web!'},
+      welcome:{title:'Welcome', desc: 'Hello, React!!'}, 
       contents: [
         {id:1, title:'HTML', desc:'HTML is for information'},
         {id:2, title:'CSS', desc:'CSS is for design'},
@@ -19,16 +21,31 @@ class App extends Component {
   }
   
   render() {
+    var _title, _desc = null;
+    if(this.state.mode === 'welcome') {
+      _title = this.state.welcome.title;
+      _desc = this.state.welcome.desc;
+    } else if(this.state.mode === 'read') {
+      _title = this.state.contents[0].title;
+      _desc = this.state.contents[0].desc;
+    }
   return (
     <div className="App">
-      <Subject 
+      {/* <Subject 
       title={this.state.subject.title} //상위 component인 App의 state를 하위 component인 Subject의 props로 준것.
       sub ={this.state.subject.sub}>
-      </Subject>
+      </Subject> */}
+      <header>
+        <h1><a href="/" onClick={function(e){{/* react에서는 이벤트 함수가 호출될 때 첫 번째 매개변수의 값으로 event라는 객체를 주입해 주기로 약속돼 있다. */}
+          console.log(e);
+          e.preventDefault();
+        }}>{this.state.subject.title}</a></h1>
+        {this.state.subject.sub}
+      </header>
       <Subject title ="REACT" sub = "For UI"></Subject>
       <TOC
       data={this.state.contents}></TOC> 
-      <Content title="HTML" desc="HTML is HyperText Markup Language."></Content>
+      <Content title={_title} desc={_desc}></Content>
     </div>
     );
   } 
