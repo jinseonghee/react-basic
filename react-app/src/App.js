c5375efc8f3b981cf5e0dfaf9c6cb171660f9ce5
jinseonghee <wlstjdgml0610@gmail.com>
2021-07-11 21:43:20 +0900

    feat - props

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,12 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
 import { Component } from 'react';
+import TOC from './components/TOC';
+import Content from './components/Content.js'
+import Subject from './components/Subject';
+
 
 class App extends Component {
   render() {
   return (
     <div className="App">
-      Hello, react!!!
+      <Subject title="WEB" sub = "world wide web!"></Subject>
+      <Subject title ="REACT" sub = "For UI"></Subject>
+      <TOC></TOC> 
+      <Content title="HTML" desc="HTML is HyperText Markup Language."></Content>
     </div>
     );
   } 
